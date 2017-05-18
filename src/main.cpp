d1845348cc4a996c4a10d12bf26569880a07e1e2
Mateusz Loskot <mateusz@loskot.net>
2017-05-18 17:31:52 +0200

    Switch to hayai supplied main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <hayai.hpp>
+#include <hayai_main.hpp>
 #include "string_append.hpp"
 #include "string_sprintf.hpp"
 #include "string_starts_with.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-    hayai::ConsoleOutputter console;
-    hayai::Benchmarker::AddOutputter(console);
-    hayai::Benchmarker::RunAllTests();
-    return 0;
+    // Set up the main runner.
+    ::hayai::MainRunner runner;
+
+    // Parse the arguments.
+    int result = runner.ParseArgs(argc, argv);
+    if (result)
+    {
+        return result;
+    }
+
+    // Execute based on the selected mode.
+    return runner.Run();
 }
