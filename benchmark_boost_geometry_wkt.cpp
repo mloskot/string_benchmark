8b5b37e06bb3da498c69f6f62e9bdde88838b930
Mateusz Łoskot <mateusz@loskot.net>
2020-03-24 01:08:12 +0100

    Switch random coordiantes to random points

--- benchmark_boost_geometry_wkt.cpp
+++ benchmark_boost_geometry_wkt.cpp
@@ -1,5 +1,5 @@
 #define CELERO_STATIC
-#include <cstdlib>
+#include <random>
 #include <string>
 #include <vector>
 #include <boost/geometry.hpp>
@@ -14,10 +14,11 @@ struct Fixture : celero::TestFixture
 {
     using point_t = boost::geometry::model::d2::point_xy<double>;
     boost::geometry::model::multi_point<point_t> multi_point_;
-    std::vector<double> coordinates_;
+    std::vector<point_t> points_;
 
     std::vector<celero::TestFixture::ExperimentValue> getExperimentValues() const override
     {
+        // Problem space as number of points (pairs of X/Y)
         std::vector<celero::TestFixture::ExperimentValue> v;
         v.emplace_back(1, 0);
         v.emplace_back(256, 0);
@@ -28,23 +29,26 @@ struct Fixture : celero::TestFixture
 
     void setUp(celero::TestFixture::ExperimentValue const& e) override
     {
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::uniform_real_distribution<> d(-180.0, 180.0);
+
         auto const size = static_cast<std::size_t>(e.Value);
-        this->coordinates_.reserve(size);
+        this->points_.reserve(size);
         for (std::size_t i = 0; i < size; i++)
         {
-            srand(unsigned(i * time(NULL)));
-            boost::geometry::append(this->multi_point_,
-                boost::geometry::make<point_t>(
-                    (double)std::rand() / (RAND_MAX),
-                    (double)std::rand() / (RAND_MAX)));
-            this->coordinates_.push_back((double)std::rand() / (RAND_MAX));
+            auto const x = d(g);
+            auto const y = d(g);
+
+            boost::geometry::append(this->multi_point_, boost::geometry::make<point_t>(x, y));
+            this->points_.emplace_back(x, y);
         }
     }
 
     void tearDown() override
     {
         multi_point_ = {};
-        coordinates_ = {};
+        points_ = {};
     }
 };
 
@@ -52,22 +56,31 @@ CELERO_MAIN
 
 BASELINE_F(wkt, to_string, Fixture, (string_benchmark::config::samples), (string_benchmark::config::iterations))
 {
-    for (auto const& v : this->coordinates_)
-        celero::DoNotOptimizeAway(std::to_string(v));
+    for (auto const& p : this->points_)
+    {
+        celero::DoNotOptimizeAway(std::to_string(p.x()));
+        celero::DoNotOptimizeAway(std::to_string(p.y()));
+    }
 }
 
 BENCHMARK_F(wkt, stringstream, Fixture, (string_benchmark::config::samples), (string_benchmark::config::iterations))
 {
     std::ostringstream oss;
-    for (auto const& v : this->coordinates_)
-        celero::DoNotOptimizeAway(oss << v);
+    for (auto const& p : this->points_)
+    {
+        celero::DoNotOptimizeAway(oss << p.x());
+        celero::DoNotOptimizeAway(oss << p.y());
+    }
 }
 
 BENCHMARK_F(wkt, lexical_cast, Fixture, (string_benchmark::config::samples), (string_benchmark::config::iterations))
 {
     std::ostringstream oss;
-    for (auto const& v : this->coordinates_)
-        celero::DoNotOptimizeAway(boost::lexical_cast<std::string>(v));
+    for (auto const& p : this->points_)
+    {
+        celero::DoNotOptimizeAway(boost::lexical_cast<std::string>(p.x()));
+        celero::DoNotOptimizeAway(boost::lexical_cast<std::string>(p.y()));
+    }
 }
 
 BENCHMARK_F(wkt, geometry_wkt, Fixture, (string_benchmark::config::samples), (string_benchmark::config::iterations))
